70d9e6414ce734d2902b417edf1101a348f02fc0
Алексей <46199980+Shot5@users.noreply.github.com>
2023-02-16 15:40:02 +0300

    check

--- inc/init.license.php
+++ inc/init.license.php
@@ -3,4 +3,27 @@ if (!defined('PONMONITOR')){
 	die('Hacking attempt!');
 }
 $PMonkey = 'free';
+$html = <<<HTML
+<title>{$metatags['title']}</title>
+<meta name="description" content="{$metatags['description']}" />
+HTML;
+$var = <<<HTML
+<script type="text/javascript">
+var root   = "/";
+var author  = "@momotuk88";
+var version  = "max10";
+</script>
+HTML;
+$ajax = <<<HTML
+<script src="../style/js/jquery-3.6.0.min.js"></script>
+<script src="../style/js/jquery.cookies.js"></script>
+<script src="../style/js/script.js"></script>
+HTML;
+$css = <<<HTML
+<link href="../style/css/styles.css" type="text/css" rel="stylesheet" />
+<link href="../style/css/engine.css" type="text/css" rel="stylesheet" />
+<link href="../style/css/nav.css" type="text/css" rel="stylesheet" />
+<link href="../style/css/font.css" type="text/css" rel="stylesheet" />
+<link href="../style/rounded/css/uicons-regular-rounded.css" rel="stylesheet">
+HTML;
 ?>
